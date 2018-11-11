5cb298a6269a01d96e040836317d4faabe3a9eb7
krzysztofSkolimowski <krzysztof.skolimowski@outlook.com>
2018-11-11 22:20:35 +0100

    add gorilla mux dependency

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
-	
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Release your imagination")
+	}).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
