165dc7626c5b18a73512f524d5354575b17e6ca9
krzysztofSkolimowski <krzysztof.skolimowski@outlook.com>
2018-11-13 22:53:44 +0100

    setup

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,11 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Release your imagination")
 	}).Methods("GET")
 	fmt.Println("Service started")
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
